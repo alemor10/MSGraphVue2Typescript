2ec8e97e8e2135080345ce76fdb0abc4ab0e29c0
Alejandro Morales <43051685+alemor10@users.noreply.github.com>
2020-11-07 17:50:04 -0800

    add variables to env

--- src/lib/microsoftGraph/config.ts
+++ src/lib/microsoftGraph/config.ts
@@ -7,8 +7,8 @@ import {
 // this can be moved somewhere else but this is where env variables are read
 
 export const useConfig = () => {
-  const REDIRECT_URI = "http://localhost:3000";
-  const AUTH_ID = "fab26eb8-2dd4-452e-a6f3-576ae15617cd";
+  const REDIRECT_URI = process.env.REDIRECT_URI
+  const AUTH_ID = process.env.AUTH_ID
 
   return {
     REDIRECT_URI,
